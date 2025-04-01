74815ec0116990bfde794dc306791e2e09cc9224
SachinM44 <sachin69778@gmail.com>
2025-04-02 00:08:02 +0530

    fix/fixed accidentally file and vercel updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true
+  }
 })
